454502d4b70ce22ca347a12a86dcf8307a618020
artistXenon <xtramainpp@gmail.com>
2023-08-16 21:03:39 +0900

    discriminate join log

--- UDP/join.go
+++ UDP/join.go
@@ -5,7 +5,7 @@ import "fmt"
 func onJoin(header *Header, body *[]byte) (res *[]byte, reply bool, err error) {
 	// TODO: parse bytes for username
 	if header != nil && body != nil {
-		fmt.Printf("%s joined lobby %s\n", header.User.Id, header.Lobby.Id)
+		fmt.Printf("join-udp: %s joined lobby %s\n", header.User.Id, header.Lobby.Id)
 		return &[]byte{0x00}, true, nil
 	} else {
 		return &[]byte{0x01}, true, nil
