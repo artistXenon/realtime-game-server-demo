5459e64860f6152b12cd2fc1743aa46e95a08486
artistXenon <xtramainpp@gmail.com>
2023-08-16 21:05:09 +0900

    calibrate ping calculation

--- state/lobby/player.go
+++ state/lobby/player.go
@@ -1,7 +1,9 @@
 package lobby
 
 import (
+	"math/rand"
 	"net"
+	"time"
 )
 
 // import "fmt"
@@ -10,6 +12,7 @@ const ping_buffer = 20
 
 type Player struct {
 	Id         string
+	InternalId string
 	Name       string
 	SessionKey string
 	UDPT       uint32
@@ -41,8 +44,16 @@ type Player struct {
 var Players = make(map[string]*Player)
 
 func CreatePlayer(id string, sessionKey string, lobby *Lobby) (player *Player) {
+	b := make([]byte, 5)
+	var randd *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for i := range b {
+		b[i] = charset[randd.Intn(len(charset))]
+	}
 	p := &Player{
 		Id:         id,
+		InternalId: string(b),
+		Name:       "Blob",
 		SessionKey: sessionKey,
 	}
 	lobby.AssignPlayer(p)
@@ -71,8 +82,11 @@ func (player *Player) AppendPing(ping int16, offset int16) {
 }
 
 func (player *Player) AppendLoss(loss int16) { // TODO: calculate loss precisely. we r getting 1 on nothing
-	player.Loss = append(player.Loss, true)
 
+	for i := int16(0); i < loss-1; i++ {
+		player.Loss = append(player.Loss, true)
+	}
+	player.Loss = append(player.Loss, false)
 	lossOverSize := len(player.Loss) - ping_buffer
 	if lossOverSize > 0 {
 		player.Loss = player.Loss[lossOverSize:]
