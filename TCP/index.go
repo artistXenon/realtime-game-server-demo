c645ebb73f35af07f76e60ef66767fd234cb5de6
artistXenon <xtramainpp@gmail.com>
2023-08-16 21:05:38 +0900

    create TCP lobby interaction

--- TCP/index.go
+++ TCP/index.go
@@ -1,11 +1,13 @@
 package TCP
 
 import (
+	"encoding/binary"
 	"fmt"
 	"inagame/state/lobby"
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
 func TCPHandler(conn net.Conn) {
@@ -14,14 +16,17 @@ func TCPHandler(conn net.Conn) {
 
 	buf := make([]byte, 1024)
 
+	log.Printf("connection made %p", conn)
+
 	for {
 		length, err := conn.Read(buf)
 		if err != nil {
 			if err == io.EOF {
-				log.Printf("connection closed")
+				log.Printf("connection closed %p", conn)
 				// connection closed
 			} else {
 				log.Printf("unresolved error: %v", err)
+				time.Sleep(5 * time.Second)
 			}
 			break
 		}
@@ -30,36 +35,61 @@ func TCPHandler(conn net.Conn) {
 			continue
 		}
 
+		var command byte
+
 		if !isPlayerBoundConnection {
-			joinResult, err := joinHandler(&buf, length)
-			if err != nil {
-				log.Printf("tcp join has failed due to error: %v", err)
-				// invalid authentication from client.
-				conn.Close()
-				return
+			command = buf[4]
+			joinResult, errcode := joinHandler(&buf, length)
+			var errMsg string
+			switch errcode {
+			case 0:
+				player = joinResult
+				if player.TCP != nil && player.TCP != &conn {
+					(*player.TCP).Write([]byte{0x00})
+					// (*player.TCP).Close()
+				}
+				player.TCP = &conn
+				isPlayerBoundConnection = true
+				fmt.Printf("join-tcp: %s joined lobby %s\n", player.Id, player.Lobby.Id)
+			case 1:
+				errMsg = "matching user/lobby not found"
+			case 2:
+				errMsg = "invalid hash on join request"
 			}
-			player = joinResult
-			player.TCP = &conn
-			isPlayerBoundConnection = true
-			fmt.Printf("%s joined lobby %s\n", player.Id, player.Lobby.Id)
-			continue
+			conn.Write([]byte{command, 0, 0, 0, 1, byte(errcode)})
+
+			if errcode != 0 {
+				log.Printf("tcp join has failed due to error: %s", errMsg)
+				// invalid authentication from client.
+				go func() {
+					time.Sleep(time.Second * 1)
+					conn.Close()
+				}()
+				return
+			} else {
+				continue
+			}
 		}
-		command := buf[0]
+		command = buf[0]
 		body := buf[1:length]
 		var res *[]byte
 		switch command {
-		case COMMAND_STATE:
-			res, err = stateHandler(&body)
+		case COMMAND_LOBBY:
+			res, err = lobbyHandler(&body, player) // todo" eat player/lobby
 		}
+
+		var resLength int32 = int32(len(*res))
+		resMeta := make([]byte, 5)
+		resMeta[0] = command
+		binary.BigEndian.PutUint32(resMeta[1:], uint32(resLength))
 
 		if err != nil {
 			log.Printf("state error: %v", err)
 		} else {
-			conn.Write(*res)
+			conn.Write(append(resMeta, *res...))
 		}
 
 		// to some stuffs for communication.
 		// conn.Write()
 	}
-
 }
---
+++ TCP/lobby.go
@@ -0,0 +1,59 @@
+package TCP
+
+import (
+	"encoding/binary"
+	"inagame/state/lobby"
+)
+
+func lobbyHandler(buf *[]byte, player *lobby.Player) (res *[]byte, err error) {
+	resBytes := &[]byte{(byte)(player.Lobby.State)}
+
+	for _, team := range player.Lobby.Teams {
+		missing := 2
+		for _, p := range team.Players {
+			playerByte := make([]byte, 30)
+			playerByte[0] = 0b1000_0000
+			if p.IsReady {
+				playerByte[0] |= 0b0100_0000
+			}
+			if p.IsLeader {
+				playerByte[0] |= 0b0010_0000
+			}
+			if p.Id == player.Id {
+				playerByte[0] |= 0b0001_0000
+			}
+
+			playerByte[1] = byte(p.Team)
+
+			copy(playerByte[2:], []byte(p.Name))
+			copy(playerByte[18:], []byte(p.InternalId))
+			binary.BigEndian.PutUint16(playerByte[23:], 0)
+			*resBytes = append(*resBytes, playerByte...)
+			missing -= 1
+		}
+		for i := 0; i < missing; i++ {
+			playerByte := make([]byte, 30)
+			playerByte[0] = 0b0000_0000
+			*resBytes = append(*resBytes, playerByte...)
+		}
+	}
+
+	return resBytes, nil
+}
+
+/*
+
+state 1
+
+player1 state 1 []
+exist
+ready
+leader
+isme
+
+player1 team 1
+player1 name 16
+player1 id 5
+player1 character 2
+
+*/
